1eb36d600a70763fe69f5113c1648d0c8d5e3144
Astroncity <erickwilliam65@gmail.com>
2024-11-16 23:50:53 -0800

    removed all rendering related items from main (except player things)

--- src/components/render.c
+++ src/components/render.c
@@ -15,5 +15,19 @@ void render_s(ecs_iter_t* it) {
 void RendererModuleImport(ecs_world_t* world) {
     ECS_MODULE(world, RendererModule);
     ECS_COMPONENT_DEFINE(world, Renderable);
-    ECS_SYSTEM_DEFINE(world, render_s, EcsOnUpdate, Renderable);
+    // ECS_SYSTEM_DEFINE(world, render_s, EcsOnUpdate, Renderable);
+
+    ecs_entity_t render_s = ecs_system_init(
+        world,
+        &(ecs_system_desc_t){
+            .entity = ecs_entity(world,
+                                 {.name = "render_s", // Name of the system
+                                  .add = ecs_ids(ecs_dependson(EcsOnUpdate))}),
+            .query.terms =
+                {
+                    {.id = ecs_id(Renderable)} // Filter for entities with Renderable
+                },
+            .callback = render_s, // Your system function
+            .quer.order_by
+        });
 }
